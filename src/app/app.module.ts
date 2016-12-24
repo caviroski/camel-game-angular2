e86c3b40ccb5dad1403a9f2779d384dfa23f4ce0
igor.caviroski <icavir@gmail.com>
2016-12-24 22:57:37 +0100

    add material design basic router function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PickerComponent } from './picker/picker.component';
 import { ScoreComponent } from './score/score.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'picker', component: PickerComponent },
+  { path: 'score', component: ScoreComponent },
+  { path: '', redirectTo: '/picker', pathMatch: 'full' },
+  { path: '**', component: PathNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PickerComponent,
-    ScoreComponent
+    ScoreComponent,
+    PathNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
