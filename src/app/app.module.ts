4074b26b4263058dacc2009aef0ebbd91f5528c2
igor.caviroski <icavir@gmail.com>
2016-12-24 17:18:38 +0100

    picker and score components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PickerComponent } from './picker/picker.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PickerComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
