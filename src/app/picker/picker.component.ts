a90af13ef9c326e6a1115ec4498488da3cd52d0a
igor.caviroski <icavir@gmail.com>
2016-12-25 23:58:22 +0100

    using html5 elements, added age, height and haircolor

--- src/app/picker/picker.component.ts
+++ src/app/picker/picker.component.ts
@@ -5,7 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './picker.component.html',
   styleUrls: ['./picker.component.css']
 })
+
 export class PickerComponent implements OnInit {
+
+  private age: any = {minAge: 14, maxAge: 70};
+  private selectedAge: string = '20';
+
+  private height: any = {minHeight: 140, maxHeight: 220};
+  private selectedHeight: string = '175';
 
   private haircolors: Array<any> = [ 
     {type: 'blonde', value: 1}, 
@@ -20,7 +27,17 @@ export class PickerComponent implements OnInit {
   ngOnInit() {
   }
 
+  private ageSelected(value: any) {
+    this.selectedAge = value;
+  }
+
+  private heightSelected(value: any) {
+    this.selectedHeight = value;
+  }
+
   private haircolorSelected(value: any) {
    console.log('Selected value is: ', value);
   }
+
+
 }
