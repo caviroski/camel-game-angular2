daa0e4f0ff1ccb5d19a3c226d920a7f4fad78edc
igor.caviroski <icavir@gmail.com>
2016-12-25 20:09:15 +0100

    remove Angular2 Material2 web components, still not ready for use

--- src/app/picker/picker.component.ts
+++ src/app/picker/picker.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickerComponent implements OnInit {
 
+  private haircolors: Array<any> = [ 
+    {type: 'blonde', value: 1}, 
+    {type: 'brown', value: 2}, 
+    {type: 'black', value: 3}, 
+    {type: 'red', value: 4}, 
+    {type: 'grey', value: 5}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  private haircolorSelected(value: any) {
+   console.log('Selected value is: ', value);
+  }
 }
